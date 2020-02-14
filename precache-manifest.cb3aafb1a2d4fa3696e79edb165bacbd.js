7620b83b119466875fe3de0745d7830738a8999e
Maheen Unzeelah <46377344+maheenunzeelah@users.noreply.github.com>
2020-02-14 21:03:52 +0500

    Updates

---
+++ precache-manifest.cb3aafb1a2d4fa3696e79edb165bacbd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51efa7d3832aefb4d4f605105bafd022",
+    "url": "/github.io/Campus-Recruitment-System/index.html"
+  },
+  {
+    "revision": "980702904f4237a44211",
+    "url": "/github.io/Campus-Recruitment-System/static/css/main.63bd36d7.chunk.css"
+  },
+  {
+    "revision": "ff104781625d9494ef3c",
+    "url": "/github.io/Campus-Recruitment-System/static/js/2.6828f6d6.chunk.js"
+  },
+  {
+    "revision": "980702904f4237a44211",
+    "url": "/github.io/Campus-Recruitment-System/static/js/main.d3167393.chunk.js"
+  },
+  {
+    "revision": "8413b76ccfbe5e642441",
+    "url": "/github.io/Campus-Recruitment-System/static/js/runtime~main.041e6a68.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/github.io/Campus-Recruitment-System/precache-manifest.cb3aafb1a2d4fa3696e79edb165bacbd.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github.io/Campus-Recruitment-System/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
